e8068b222041cf68dc2632ca1b107f6d23ec00c4
Maxim Korolyov <mkorolyov@humans.net>
2019-12-02 11:34:39 +0300

    added duplicated err checker

--- error.go
+++ error.go
@@ -4,7 +4,7 @@ import (
 	"github.com/jackc/pgconn"
 )
 
-func Duplicated(err error) bool {
+func IsDuplicated(err error) bool {
 	// unique_violation
 	if pgErr, ok := err.(*pgconn.PgError); ok && pgErr.Code == "23505" {
 		return true
